9c40fe0777c997d44ab95bb36f9afbef2c66c00a
Adrian Taylor <adetaylor@chromium.org>
2021-08-15 20:11:49 -0700

    Adding test code

--- src/test.cc
+++ src/test.cc
@@ -1,5 +1,6 @@
 #include "autocxx-subclass-demo/include/test.h"
 #include "autocxx-subclass-demo/src/main.rs.h"
+#include "autocxx-subclass-demo/src/test.rs.h"
 
 WebContentsObserver::WebContentsObserver() {}
 WebContentsObserver::WebContentsObserver(WebContents* wc) : web_contents_(wc) {}
@@ -28,4 +29,29 @@ void MyWebContentsObserverCpp::RenderFrameDeleted(RenderFrameHost* rfh) {
 
 void MyWebContentsObserverCpp::MyWebContentsObserverCpp_remove_ownership() {
   MyWebContentsObserver_RelinquishOwnership(*obs);
+}
+
+
+/// And the same for our test code
+
+
+MyTestObserverCpp::MyTestObserverCpp(rust::Box<MyTestObserverHolder> _obs) : obs(std::move(_obs)) {
+  mark_allocated();
+}
+
+MyTestObserverCpp::~MyTestObserverCpp() {
+  mark_freed();
+}
+
+
+void MyTestObserverCpp::a() {
+  MyTestObserver_a(*obs);
+}
+
+std::unique_ptr<MyTestObserverCpp> MyTestObserverCpp_make_unique(rust::Box<MyTestObserverHolder> _obs) {
+  return std::make_unique<MyTestObserverCpp>(std::move(_obs));
+}
+
+void MyTestObserverCpp::MyTestObserverCpp_remove_ownership() {
+   MyTestObserver_RelinquishOwnership(*obs);
 }
